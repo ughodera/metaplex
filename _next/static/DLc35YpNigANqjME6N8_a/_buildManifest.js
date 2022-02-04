51a9dd86a7e687548dcd44265a5c95fe4aa61213
Urvesh Ghoderao <urveshghoderao@Urveshs-MacBook-Pro.local>
2022-02-04 15:01:53 +0530

    Updates

--- _next/static/DLc35YpNigANqjME6N8_a/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-c51bc0b1e2597019c6ea.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
